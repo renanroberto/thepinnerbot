a7926e88bf3ee1cd075f2913287b4b6733d05293
Renan <renanroberto1@gmail.com>
2020-06-02 15:01:52 -0300

    initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for thepinnerbot
+
+## Unreleased changes
---
+++ src/BotCore.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveAnyClass #-}
+
+module BotCore (bot, Update) where
+
+import GHC.Generics
+import System.Environment (lookupEnv)
+import Data.Maybe (fromMaybe)
+import Network.Wreq (post)
+import Data.Aeson
+  ( ToJSON
+  , FromJSON
+  , toJSON
+  , parseJSON
+  , fieldLabelModifier
+  , genericParseJSON
+  , defaultOptions
+  , genericToJSON
+  )
+
+
+data Chat = Chat
+  { chat_id :: Int
+  , chat_type :: String
+  } deriving (Generic, Show)
+
+instance ToJSON Chat where
+  toJSON = genericToJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "chat_" }
+
+instance FromJSON Chat where
+  parseJSON = genericParseJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "chat_" }
+
+
+data User = User
+  { user_id :: Int
+  , user_first_name :: String
+  } deriving (Generic, Show)
+
+instance ToJSON User where
+  toJSON = genericToJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "user_" }
+
+instance FromJSON User where
+  parseJSON = genericParseJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "user_" }
+
+
+data Message = Message
+  { message_message_id :: Int
+  , message_from :: Maybe User
+  , message_date :: Int
+  , message_chat :: Chat
+  , message_text :: Maybe String
+  } deriving (Generic, Show)
+
+instance ToJSON Message where
+  toJSON = genericToJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "message_" }
+
+instance FromJSON Message where
+  parseJSON = genericParseJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "message_" }
+
+
+data Update = Update
+  { update_update_id :: Int
+  , update_message :: Maybe Message
+  } deriving (Generic, Show)
+
+instance ToJSON Update where
+  toJSON = genericToJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "update_" }
+
+instance FromJSON Update where
+  parseJSON = genericParseJSON defaultOptions
+    { fieldLabelModifier = dropPrefix "update_" }
+
+
+data Telegram = Telegram
+  { ok :: Bool
+  , result :: [Update]
+  } deriving (Generic, ToJSON, FromJSON)
+
+
+dropPrefix :: String -> String -> String
+dropPrefix prefix str = drop (length prefix) str
+
+
+getToken :: IO String
+getToken = do
+  env <- lookupEnv "TOKEN"
+  return $ fromMaybe "" env
+
+api :: String -> String -> String
+api token method = "https://api.telegram.org/bot" ++ token ++ method
+
+
+bot :: Update -> IO ()
+bot update =
+  case update_message update of
+    Nothing -> return ()
+    Just message -> return ()
